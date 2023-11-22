e69d43cc395089939b80048faa84c88ebb9cb939
Zemyt <tijme_@hotmail.com>
2023-11-22 16:58:49 +0100

    add a simple test

--- tests/test_.py
+++ tests/test_.py
@@ -1,15 +1,23 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+# from selenium import webdriver
+# from selenium.webdriver.chrome.options import Options
 
 
-def test_page_title():
-    options = Options()
-    options.add_argument("--headless")
-    driver = webdriver.Chrome(options=options)
+# def test_page_title():
+#     options = Options()
+#     options.add_argument("--headless")
+#     driver = webdriver.Chrome(options=options)
 
-    driver.get("http://127.0.0.1:4000/")
+#     driver.get("http://127.0.0.1:4000/")
 
-    expected_title = "Home"
-    assert driver.title == expected_title
+#     expected_title = "Home"
+#     assert driver.title == expected_title
 
-    driver.quit()
+#     driver.quit()
+
+
+def test_addition():
+    result = 2 + 2
+
+    expected_result = 4
+
+    assert result == expected_result
